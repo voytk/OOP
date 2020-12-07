437d25484d18b259075dac07617e278c589d58fd
Alice Voytkevich <71379424+voytk@users.noreply.github.com>
2020-12-07 19:38:24 +0200

    v1

---
+++ A.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp30
+{
+    public class A
+    {
+        private int[] arr;
+
+        public int Length
+        {
+            get
+            {
+                return arr.Length;
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+
+
+    }
+}
